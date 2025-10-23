3910be083751b00de92b58b93dc0c48bb509f208
allanrobertomolto <allanrobertomolto@hotmail.com>
2025-10-23 09:55:54 -0300

    Emulador de sensor

---
+++ EmulacaoDados.py
@@ -0,0 +1,27 @@
+#Escrita
+import random
+import csv
+
+ARQUIVO = "log_medidas.csv"  # nome do arquivo gerado
+N = 20                       # quantidade de amostras
+
+# Geração dos valores simulados
+correntes = [round(random.uniform(0.5, 10.0), 2) for _ in range(N)]
+tensoes = [round(random.uniform(110.0, 127.0), 2) for _ in range(N)]
+
+# Escrita no arquivo CSV
+with open(ARQUIVO, "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f) #Cria o objeto que escreve no arquivo CSV
+    writer.writerow(["Amostra", "Corrente (A)", "Tensão (V)"])
+    for i in range(N):
+        writer.writerow([i + 1, correntes[i], tensoes[i]])
+
+print(f"Arquivo '{ARQUIVO}' gerado com sucesso!")
+
+#Leitura
+import csv
+
+with open("log_medidas.csv", "r", encoding="utf-8") as f:
+    leitor = csv.DictReader(f)
+    for linha in leitor:
+        print(f"Amostra {linha['Amostra']}: {linha['Corrente (A)']} A, {linha['Tensão (V)']} V")
